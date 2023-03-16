4abdde1ca1d5137a7f64fb0e50ec5aad71bc27a6
Alexandr Makarov <makarena2009@yandex.ru>
2023-03-16 17:45:55 +0300

    22

--- work08.py
+++ work08.py
@@ -36,23 +36,23 @@
 # (или список слов, если таковых несколько).
 
 
-import os
+# import os
  
  
-def longest_words(file):
-    with open(file, "r") as text:
-        words = text.read().split()
+# def longest_words(file):
+#     with open(file, "r") as text:
+#         words = text.read().split()
  
-    max_length = len(max(words, key=len))
-    sought_words = [word for word in words if len(word) == max_length]
+#     max_length = len(max(words, key=len))
+#     sought_words = [word for word in words if len(word) == max_length]
  
-    f = "result.txt"
-    i = 1
-    while os.path.exists(f):
-        f = f"result{i}.txt"
-        i += 1
-    with open(f, "w") as file:
-        file.write(" ".join(sought_words) + "\n")
+#     f = "result.txt"
+#     i = 1
+#     while os.path.exists(f):
+#         f = f"result{i}.txt"
+#         i += 1
+#     with open(f, "w") as file:
+#         file.write(" ".join(sought_words) + "\n")
  
  
-longest_words("article.txt")
+# longest_words("article.txt")

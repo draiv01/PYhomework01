7652446e64fa953e4642c5e0023b7196486e6608
Alexandr Makarov <makarena2009@yandex.ru>
2023-03-16 17:41:54 +0300

    16

--- work08.py
+++ work08.py
@@ -4,16 +4,55 @@
 # Протестируем функцию на файле «article.txt» со следующим содержимым:
 
 
-def read_last(lines, file):
-    if lines > 0:
-        with open(file, encoding='utf-8') as text:
-            file_lines = text.readlines()[-lines:]
-        for line in file_lines:
-            print(line.strip())
-        else:
-            print('Количество строк может быть только целым положительным')
+# def read_last(lines, file):
+#     if lines > 0:
+#         with open(file, encoding="utf-8") as text:
+#             file_lines = text.readlines()[-lines:]
+#         for line in file_lines:
+#             print(line.strip())
+#         else:
+#             print("end")
  
  
-# Тесты
-read_last(3, 'article.md')
-read_last(-5, 'article.md')
+
+# read_last(3, "article.txt")
+# read_last(-5, "article.txt")
+
+
+
+
+# Документ «article.txt» содержит следующий текст:
+# Вечерело
+# Жужжали мухи
+# Светил фонарик
+# Кипела вода в чайнике
+# Венера зажглась на небе
+# Деревья шумели
+# Тучи разошлись
+# Листва зеленела
+
+# Требуется реализовать функцию longest_words(file),
+# которая записывает в файл result.txt слово, имеющее максимальную длину
+# (или список слов, если таковых несколько).
+
+
+import os
+ 
+ 
+def longest_words(file):
+    with open(file, "r") as text:
+        words = text.read().split()
+ 
+    max_length = len(max(words, key=len))
+    sought_words = [word for word in words if len(word) == max_length]
+ 
+    f = "result.txt"
+    i = 1
+    while os.path.exists(f):
+        f = f"result{i}.txt"
+        i += 1
+    with open(f, "w") as file:
+        file.write(" ".join(sought_words) + "\n")
+ 
+ 
+longest_words("article.txt")
